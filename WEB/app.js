23148147916cd4e62279defde43725aae9807466
Jon Martin Mikalsen <overmind_Zerg6@hotmail.com>
2014-10-09 14:39:23 +0200

    Web server can now handle connections against nodes

--- WEB/app.js
+++ WEB/app.js
@@ -10,6 +10,8 @@ var express = require('express')
   , requests = require('./public/javascripts/lab4.js').Request
 
 var app = module.exports = express.createServer();
+var stringify = require('json-stable-stringify');
+var WebSocket = require('ws')
 
 //Modify param function to take in regex
 app.param(function(name, fn){
@@ -33,7 +35,7 @@ app.configure(function(){
   app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-app.use(express.compiler({ src : __dirname + '/public', enable: ['less']}));
+  app.use(express.compiler({ src : __dirname + '/public', enable: ['less']}));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -74,25 +76,109 @@ app.get('/lab4/:node/storage', function(req, res) {
 app.post('/lab4/:node/storage', function(req, res){
 	//res.send("POST to storage: " + req.body.newVal)
 	console.log("Post: " + req.params.node[0] + " : " + req.body.newVal)
-	res.render('lab4show', { title: "Post done", node_addr: req.params.node[0], newVal: req.body.newVal, socket_action: 1 })
+	//res.render('lab4show', { title: "Post done", node_addr: req.params.node[0], newVal: req.body.newVal, socket_action: 1 })
+	var msg = {
+		type: "message",
+	    text: "test sending POST",
+	    id:   1,
+	    date: Date.now()
+	}
+	try {
+		var ws = new WebSocket('ws://'+ req.params.node[0] + '/node');
+		ws.on('open', function() {
+		    ws.send(stringify(msg));
+		});
+		ws.on('message', function(message) {
+		    console.log('received: %s', message);
+		    res.send(message)
+		});
+		ws.on('error', function(error) {
+			res.send(error)
+		})
+	} catch(e) {
+		console.log('Error: %s', e);
+	}
 });
 
 app.put('/lab4/:node/storage/:key', function(req, res){
 	//res.send("UPDATE to storage key: " + req.body.newVal)
 	console.log("Post: " + req.params.node[0] + " : " + req.body.newVal)
-	res.render('lab4put', { title: "Put done", node_addr: req.params.node[0], key: req.params.key[0], newVal: req.body.newVal, socket_action: 2 })
+	//res.render('lab4put', { title: "Put done", node_addr: req.params.node[0], key: req.params.key[0], newVal: req.body.newVal, socket_action: 2 })
+	var msg = {
+		type: "message",
+	    text: "test sending PUT",
+	    id:   1,
+	    date: Date.now()
+	}
+	try {
+		var ws = new WebSocket('ws://'+ req.params.node[0] + '/node');
+		ws.on('open', function() {
+		    ws.send(stringify(msg));
+		});
+		ws.on('message', function(message) {
+		    console.log('received: %s', message);
+		    res.send(message)
+		});
+		ws.on('error', function(error) {
+			res.send(error)
+		})
+	} catch(e) {
+		console.log('Error: %s', e);
+	}
 });
 
 app.del('/lab4/:node/storage/:key', function(req, res){
 	//res.send("DELETE to storage key: " + req.params.key[0])
 	console.log("Post: " + req.params.node[0] + " : " + req.body.newVal)
-	res.render('lab4del', { title: "Delete done", node_addr: req.params.node[0], key: req.params.key[0], socket_action: 3 })
+	//res.render('lab4del', { title: "Delete done", node_addr: req.params.node[0], key: req.params.key[0], socket_action: 3 })
+	var msg = {
+		type: "message",
+	    text: "test sending DELETE",
+	    id:   1,
+	    date: Date.now()
+	}
+	try {
+		var ws = new WebSocket('ws://'+ req.params.node[0] + '/node');
+		ws.on('open', function() {
+		    ws.send(stringify(msg));
+		});
+		ws.on('message', function(message) {
+		    console.log('received: %s', message);
+		    res.send(message)
+		});
+		ws.on('error', function(error) {
+			res.send(error)
+		})
+	} catch(e) {
+		console.log('Error: %s', e);
+	}
 });
 
 app.get('/lab4/:node/storage/:key', function(req, res){
-	res.send(requests.DoNodeGet(req.params.node[0], req.params.key[0]));
+	//res.send(requests.DoNodeGet(req.params.node[0], req.params.key[0]));
 	//res.send('ip: ' + req.params.node[0])
 	//res.render('lab4get', { title: "Get" , node_addr: req.params.node[0], key: req.params.key[0] });//, layout: false });
+	var msg = {
+			type: "message",
+		    text: "test sending GET: " + req.params.key[0],
+		    id:   1,
+		    date: Date.now()
+	}
+	try {
+		var ws = new WebSocket('ws://'+ req.params.node[0] + '/node');
+		ws.on('open', function() {
+		    ws.send(stringify(msg));
+		});
+		ws.on('message', function(message) {
+		    console.log('received: %s', message);
+		    res.send(message)
+		});
+		ws.on('error', function(error) {
+			res.send(error)
+		})
+	} catch(e) {
+		console.log('Error: %s', e);
+	}
 });
 
 
--- WEB/routes/lab4/show.js
+++
@@ -1,3 +0,0 @@
-exports.show = function(req, res){
-	res.render('lab4show', { title: 'Lab 4 Show', node_addr: req.params.node })
-});
